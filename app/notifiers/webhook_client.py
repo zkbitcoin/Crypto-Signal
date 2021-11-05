ee64856621d91fdb1e8a7d10da081658741b19b7
evaluatein <evaluatein@gmail.com>
2021-11-05 06:25:22 -0400

    update

--- app/notifiers/webhook_client.py
+++ app/notifiers/webhook_client.py
@@ -8,7 +8,21 @@ import requests
 import structlog
 
 from notifiers.utils import NotifierUtils
+import asyncio
+import websockets
 
+async def ws_send(data):
+    uri = "ws://127.0.0.1:3000"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps({
+            "event": "subscribe",
+            "subscription": "crypto_signal.crypto_signal"
+        }))
+        resp = await websocket.recv()
+        print(f"<<< {resp}")
+        await websocket.send(data)
+        resp = await websocket.recv()
+        print(f"<<< {resp}")
 
 class WebhookNotifier(NotifierUtils):
     """Class for handling webhook notifications
@@ -30,10 +44,22 @@ class WebhookNotifier(NotifierUtils):
         #market_pair = market_pair.replace('/', '_').lower()
         #chart_file = '{}/{}_{}_{}.png'.format('./charts', exchange, market_pair, candle_period)
 
-        data = {'messages': messages}
-        data = json.dumps(data)
+        #data = {'messages': messages}
+        #data = json.dumps(data)
         #Content type must be included in the header
         headers = {"content-type": "application/json"}
+
+        asyncio.run(ws_send(json.dumps({
+                "event": "message",
+                "subscription": "crypto_signal.crypto_signal",
+                "data": {
+                    "messages": messages
+                }
+        })))
+        #
+        #result = ws.recv()
+        #result = json.loads(result)
+        #print ("Received '%s'" % result)
 
         #if chart_file and os.path.exists(chart_file):
         #    files = {'chart': open(chart_file, 'rb')}
@@ -44,12 +70,12 @@ class WebhookNotifier(NotifierUtils):
         #    else:
         #        request = requests.post(self.url, files=files, data=data)
         #else:
-        if self.username and self.password:
-            request = requests.post(
-                self.url,  headers=headers, data=data, auth=(self.username, self.password))
-        else:
-            request = requests.post(self.url,  headers=headers, data=data)
+        #if self.username and self.password:
+        #    request = requests.post(
+        #        self.url,  headers=headers, data=data, auth=(self.username, self.password))
+        #else:
+        #    request = requests.post(self.url,  headers=headers, data=data)
 
-        if not request.status_code == requests.codes.ok:
-            self.logger.error("Request failed: %s - %s",
-                              request.status_code, request.content)
+        #if not request.status_code == requests.codes.ok:
+        #    self.logger.error("Request failed: %s - %s",
+        #                      request.status_code, request.content)
---
+++ run-local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source .venv/bin/activate
+cd app
+python3 app.py 
